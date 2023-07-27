93dce2b9458d95b4f80e67de3e3f72bff8290d6d
Teon Ooi <teon@protonmail.com>
2023-07-27 21:53:32 +0800

    Update to display tasks and task action.

--- app/Http/Livewire/Todo.php
+++ app/Http/Livewire/Todo.php
@@ -6,8 +6,24 @@ use Livewire\Component;
 
 class Todo extends Component
 {
+
+    public $tasks = [];
+
+    public $task = '';
+
     public function render()
     {
         return view('livewire.todo');
     }
+
+    public function addTask()
+    {
+        array_push($this->tasks, [
+            'id'    => uniqid(),
+            'label' => $this->task,
+        ]);
+
+        $this->task = '';
+    }
+
 }
--- resources/views/livewire/todo.blade.php
+++ resources/views/livewire/todo.blade.php
@@ -13,6 +13,22 @@
             <div class="card">
                 <div class="card-body p-5">
 
+                    <form class="d-flex mb-1">
+                        <input wire:model="task" type="text" class="form-control me-2" placeholder="New task..." />
+                        <button type="submit" disabled class="d-none">Prevent form submit from enter</button>
+                        <button wire:click.prevent="addTask" class="btn btn-primary"><i class="fas fa-plus"></i></button>
+                    </form>
+
+                    <div class="list-group mt-3">
+                        @forelse($tasks as $task)
+                            <div class="list-group-item list-group-item-action d-flex align-items-center">
+                                {{ $task[ 'label' ] }}
+                            </div>
+                        @empty
+                            <p class="mt-4 text-center">Hooray! You don't have any pending task.</p>
+                        @endforelse
+                    </div>
+
                 </div>
             </div>
         </div>
