2658e99dafb9cffb3cfd508ac46e953e6dc86d9f
Teon Ooi <teon@protonmail.com>
2023-07-27 08:37:31 +0800

    Create todo component and base layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <title>Livewire Todo</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <link href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-        @livewireStyles
+        <livewire:styles />
     </head>
     <body>
         <main>
@@ -14,7 +14,7 @@
                 <header class="pb-3 mb-4">
                 </header>
 
-                @yield('content')
+                 <livewire:todo />
 
                 <footer class="pt-3 mt-4 text-body-secondary">
                 </footer>
@@ -22,6 +22,6 @@
         </main>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-        @livewireScripts
+        <livewire:scripts />
     </body>
 </html>
