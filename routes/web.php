cba52ecb256e15b4fe66afe4e25b434d9f10a795
Teon Ooi <teon@protonmail.com>
2023-08-01 09:16:17 +0800

    Change root route.

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('todo');
 });
 
 Route::get('/hello', function () {
     return view('hello');
-});
-
-Route::get('/todo', function () {
-    return view('todo');
 });
