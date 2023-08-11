c209e7b0a66615c7452bbd12a28b23eef594b050
Teon Ooi <teon@protonmail.com>
2023-08-11 22:37:44 +0800

    Remove welcome and hello world.

--- resources/views/livewire/hello.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
-        <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold">{{ $title }}</h1>
-            <p class="col-md-8 fs-4">{{ $description }}</p>
-        </div>
-    </div>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('todo');
 });
-
-Route::get('/hello', function () {
-    return view('hello');
-});
